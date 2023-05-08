670f407c557657dfd570c246d5dd9a119556b684
chen <602042954@qq.com>
2023-05-08 17:38:52 +0800

    jdbc

--- csmall-product/src/test/java/cn/tedu/csmall/product/CsmallProductApplicationTests.java
+++ csmall-product/src/test/java/cn/tedu/csmall/product/CsmallProductApplicationTests.java
@@ -1,13 +1,20 @@
 package cn.tedu.csmall.product;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 @SpringBootTest
 class CsmallProductApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+    @Autowired
+    DataSource dataSource;
 
+    @Test
+    void getConnection() throws SQLException {
+        dataSource.getConnection();
+    }
 }
