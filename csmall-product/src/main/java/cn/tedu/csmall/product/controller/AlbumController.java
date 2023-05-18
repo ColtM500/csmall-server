cafa57dc9ac9dcb00fab411b2b195010544d1c51
chen <602042954@qq.com>
2023-05-18 15:41:04 +0800

    AlbumController

--- csmall-product/src/main/java/cn/tedu/csmall/product/controller/AlbumController.java
+++ csmall-product/src/main/java/cn/tedu/csmall/product/controller/AlbumController.java
@@ -5,12 +5,19 @@ import cn.tedu.csmall.product.pojo.param.AlbumAddNewParam;
 import cn.tedu.csmall.product.service.IAlbumService;
 import com.github.xiaoymin.knife4j.annotations.ApiOperationSupport;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
+@Slf4j
 @RestController
 @RequestMapping("/album")
 @Api(tags = "04.相册管理模块")
@@ -19,9 +26,17 @@ public class AlbumController {
     @Autowired
     private IAlbumService albumService;
 
+    @ExceptionHandler
+    public String handleServiceException(ServiceException e){
+        log.warn("程序运行过程中出现了ServiceException, 将统一处理！");
+        log.warn("异常信息:{}", e.getMessage());
+        return  e.getMessage();
+    }
+
     // http://localhost:8080/album/add-new?name=TestName001&description=TestDescription001&sort=99
     @PostMapping("/add-new")
     @ApiOperation("添加相册")
+    @ApiOperationSupport(order = 100)
     public String addNew(AlbumAddNewParam albumAddNewParam){
         try {
             albumService.addNew(albumAddNewParam);
@@ -33,10 +48,15 @@ public class AlbumController {
         }
     }
 
+    // http://localhost:8080/album/delete
     @PostMapping("/delete")
     @ApiOperation("根据ID删除相册")
     @ApiOperationSupport(order = 200)
-    public String delete(){
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "albumId", value = "相册ID", required = true, dataType = "long"),
+            @ApiImplicitParam(name = "userId", value = "用户ID", required = true, dataType = "long")
+    })
+    public String delete(Long albumId, Long userId){
         throw new RuntimeException("别急，还没做!");
     }
 }
