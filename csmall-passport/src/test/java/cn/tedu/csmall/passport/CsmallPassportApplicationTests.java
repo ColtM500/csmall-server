9253b068540a084d0ff5f7d93cf668349db8f200
chen <602042954@qq.com>
2023-05-24 14:36:45 +0800

    passport

--- csmall-passport/src/test/java/cn/tedu/csmall/passport/CsmallPassportApplicationTests.java
+++ csmall-passport/src/test/java/cn/tedu/csmall/passport/CsmallPassportApplicationTests.java
@@ -1,13 +1,23 @@
 package cn.tedu.csmall.passport;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
 
 @SpringBootTest
 class CsmallPassportApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void getConnection() throws Throwable{
+        dataSource.getConnection();
+    }
 }
