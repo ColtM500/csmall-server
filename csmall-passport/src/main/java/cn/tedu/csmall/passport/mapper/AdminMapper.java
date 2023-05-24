292a86aba857affddfe0b5a94b67ac66b3335243
chen <602042954@qq.com>
2023-05-24 14:39:37 +0800

    passport

---
+++ csmall-passport/src/main/java/cn/tedu/csmall/passport/mapper/AdminMapper.java
@@ -0,0 +1,9 @@
+package cn.tedu.csmall.passport.mapper;
+
+import cn.tedu.csmall.passport.pojo.entity.Admin;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AdminMapper extends BaseMapper<Admin> {
+}
