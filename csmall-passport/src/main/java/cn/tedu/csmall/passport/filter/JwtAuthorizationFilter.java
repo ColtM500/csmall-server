590937340dcc20064433074bbb8948989d756d34
chen <602042954@qq.com>
2023-05-27 14:12:26 +0800

    try catch

--- csmall-passport/src/main/java/cn/tedu/csmall/passport/filter/JwtAuthorizationFilter.java
+++ csmall-passport/src/main/java/cn/tedu/csmall/passport/filter/JwtAuthorizationFilter.java
@@ -1,6 +1,8 @@
 package cn.tedu.csmall.passport.filter;
 
 import cn.tedu.csmall.passport.security.LoginPrincipal;
+import cn.tedu.csmall.passport.web.JsonResult;
+import cn.tedu.csmall.passport.web.ServiceCode;
 import com.alibaba.fastjson.JSON;
 import com.sun.corba.se.spi.ior.ObjectKey;
 import io.jsonwebtoken.*;
@@ -20,6 +22,7 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -57,25 +60,68 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
         //尝试解析JWT
         response.setContentType("application/json; charset=utf-8");
         Claims claims = null;
+        try {
             claims = Jwts
             .parser()
             .setSigningKey(secretKey)
             .parseClaimsJws(jwt)
             .getBody();
+        } catch (ExpiredJwtException e) {
+            String message = "您的登录信息已经过期，请重新登录！";
+            log.warn("程序运行过程中出现了ExpiredJwtException，将向客户端响应错误信息！");
+            log.warn("错误信息：{}", message);
+            //此处为什么要加fastjson依赖？因为要传递字符串发送到前端时需要以json形式 此时需要加此依赖转换（反之json也可转为字符串）
+            JsonResult jsonResult = JsonResult.fail(ServiceCode.ERR_JWT_EXPIRED, message);
+            String s = JSON.toJSONString(jsonResult);//将字符串对象放进去放入 转变为Json格式的字符串
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println(s);//显示一串？？？ java内部的编码未ISO-8859-1 不支持中文编码这样
+            printWriter.close();
+            return;
+        } catch (MalformedJwtException e) {
+            String message = "非法访问!";
+            log.warn("程序运行过程中出现了MalformedJwtException，将向客户端响应错误信息！");
+            log.warn("错误信息:{}",message);
+            //此处为什么要加fastjson依赖？因为要传递字符串发送到前端时需要以json形式 此时需要加此依赖转换（反之json也可转为字符串）
+            JsonResult jsonResult = JsonResult.fail(ServiceCode.ERR_JWT_MALFORMED, message);
+            String s = JSON.toJSONString(jsonResult);//将字符串对象放进去放入 转变为Json格式的字符串
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println(s);//显示一串？？？ java内部的编码未ISO-8859-1 不支持中文编码这样
+            printWriter.close();
+            return;
+        } catch (SignatureException e) {
+            String message = "非法访问!";
+            log.warn("程序运行过程中出现了SignatureException，将向客户端响应错误信息！");
+            log.warn("错误信息：{}", message);
+            //此处为什么要加fastjson依赖？因为要传递字符串发送到前端时需要以json形式 此时需要加此依赖转换（反之json也可转为字符串）
+            JsonResult jsonResult = JsonResult.fail(ServiceCode.ERR_JWT_SIGNATURE, message);
+            String s = JSON.toJSONString(jsonResult);//将字符串对象放进去放入 转变为Json格式的字符串
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println(s);//显示一串？？？ java内部的编码未ISO-8859-1 不支持中文编码这样
+            printWriter.close();
+            return;
+        } catch (Throwable e) {
+            String message = "服务器忙，请稍后再试！【在开发过程中，如果看到此提示，应该检查服务器端的控制台，分析异常，并在解析JWT的过滤器中补充处理对应异常的代码块】";
+            log.warn("程序运行过程中出现了Throwable，将向客户端响应错误信息！");
+            log.warn("异常：", e);
+            //此处为什么要加fastjson依赖？因为要传递字符串发送到前端时需要以json形式 此时需要加此依赖转换（反之json也可转为字符串）
+            JsonResult jsonResult = JsonResult.fail(ServiceCode.ERR_UNKNOWN, message);
+            String s = JSON.toJSONString(jsonResult);//将字符串对象放进去放入 转变为Json格式的字符串
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println(s);//显示一串？？？ java内部的编码未ISO-8859-1 不支持中文编码这样
+            printWriter.close();
+            return;
+        }
 
         //尝试获取JWT中的数据
         Long id = claims.get("id", Long.class);
         String username = claims.get("username", String.class);
         String authoritiesJsonString = claims.get("authoritiesJsonString", String.class);
+
         System.out.println("id="+id);
         System.out.println("username="+username);
 
-        //创建当事人数据
-        LoginPrincipal loginPrincipal = new LoginPrincipal()
-                .setId(id).setUsername(username);
-
         //创建认证信息
-        Object principal = loginPrincipal; //可以是任意类型,暂时使用用户名
+        Object principal = new LoginPrincipal().setId(id).setUsername(username);
         Object credentials = null;  //本次不需要
         Collection<SimpleGrantedAuthority> authorities =
                 JSON.parseArray(authoritiesJsonString, SimpleGrantedAuthority.class);
